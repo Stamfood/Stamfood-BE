69eebd496095b9af73e1458751eb3d9883f39db5
JeYeongR <wpdud2003@gmail.com>
2023-08-13 17:16:43 +0900

    Init: #1 프로젝트 초기 셋팅

---
+++ src/main/java/kr/kro/stamfood/StamfoodApplication.java
@@ -0,0 +1,13 @@
+package kr.kro.stamfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StamfoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StamfoodApplication.class, args);
+    }
+
+}
