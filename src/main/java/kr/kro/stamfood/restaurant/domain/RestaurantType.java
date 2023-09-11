6a132bef97fc6ee2024a66a31025d6585e8ec3e9
lala9663 <lala96632040@gmail.com>
2023-09-11 14:50:43 +0900

    refactor: #7 res_deleted 컬럼명 오타

---
+++ src/main/java/kr/kro/stamfood/restaurant/domain/RestaurantType.java
@@ -0,0 +1,19 @@
+package kr.kro.stamfood.restaurant.domain;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+public class RestaurantType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long resTypeId;
+    @Column(name = "type_name", columnDefinition = "VARCHAR(10)")
+    private String resType;
+}
