7861d5621a2974eff11d50ce30b31cb9a14e531b
lala9663 <lala96632040@gmail.com>
2023-09-11 15:04:32 +0900

    feat: #10 restaurant 등록 api 및 예외처리 클래스 생성

---
+++ src/main/java/kr/kro/stamfood/restaurant/persistence/RestaurantJpaRepository.java
@@ -0,0 +1,11 @@
+package kr.kro.stamfood.restaurant.persistence;
+
+import kr.kro.stamfood.restaurant.domain.Restaurant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantJpaRepository extends JpaRepository<Restaurant, Long> {
+    boolean existsByResName(String resName);
+
+}
